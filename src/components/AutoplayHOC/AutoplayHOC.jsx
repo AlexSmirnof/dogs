4671b2760c390da3fdc34f26c22221ed1a456a6c
AlexSmirnof <moonlight_by@mail.ru>
2018-06-28 22:45:26 +0300

    autoplay fixed

--- src/components/AutoplayHOC/AutoplayHOC.jsx
+++ src/components/AutoplayHOC/AutoplayHOC.jsx
@@ -10,12 +10,14 @@ const AutoplayHOC = ViewedComponent =>
 
         componentDidMount(){
             if(this.props.autoplay){
-                this.clear = setInterval(this.next, 3500);
+                this.clear = setInterval(this.play, 3000);
             }
         }
         componentWillUnmount(){
             this.clear && clearInterval(this.clear);
         }
+
+        play = () => this.props.autoplay && this.next();
 
     }
 
